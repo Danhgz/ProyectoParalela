3f1836080554a15f0d4e4b37402c54197023e01d
Danhgz <43118660+Danhgz@users.noreply.github.com>
2019-10-23 21:09:07 -0600

    Version Final?

--- ProyectoK-Means.cpp
+++ ProyectoK-Means.cpp
@@ -1,5 +1,3 @@
-// ProyectoK-Means.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-
 #include <omp.h>
 #include <conio.h>
 #include <time.h>
@@ -46,7 +44,6 @@ double generarDouble(int min, int max) {
 	double r = random * diff;
 	return (double)min + r;
 }
-
 int generarInt(int min, int max) {
 	int randm = 0;
 	randm = rand() % (max - min)+min;
@@ -76,7 +73,6 @@ double calcDisMin(vector<double>& puntoEnX, vector<vector<double>>& vectorCentro
 	}
 	return min;
 }
-
 int calcMinPos(vector<double>& puntoEnX, vector<vector<double>>& vectorCentroides) { //Posicion donde esta la distancia minima
 	int posMin = 0;
 	double aux = 0.0;
@@ -102,18 +98,15 @@ double calcularProb(vector<double>& puntoEnX, vector<vector<double>>& vectorCent
 }
 
 //calcular probabilidades de cada punto
-void probabilidadesPuntos(vector<vector<double>>& vectorDatos, vector<vector<double>>& vectorCentroides, vector<double>& probabilidades, double psi, double l) {
+void probabilidadesPuntos(vector<vector<double>>& vectorDatos, vector<vector<double>>& vectorCentroides, vector<double>& probabilidades, double psi, int hilos, double l) {
 	double prob = 0.0;
 	int tamano = vectorDatos.size();
+	#pragma omp parallel for num_threads(hilos) shared(l)
 	for (int i = 0; i < tamano; ++i) {
 		prob = l * calcularProb(vectorDatos[i], vectorCentroides, psi);
-		probabilidades.push_back(prob);
+		probabilidades[i] = prob;
 	}
 }
-
-//****************************************************************************************
-//calcular en probabilidad
-//recalcular costo de puntos con cada centroide, debe ser el minimo. d(x,C) distancia de x al centroide mas cercano en C
 
 int compararVectores(vector<double> vec1, vector<double> vec2) {
 	int iguales = 1;
@@ -136,7 +129,7 @@ int validarCentroide(vector<vector<double>>& centroides, vector<double>& unX) { 
 	return valido;
 }
 
-void reclusterKmeans2(vector<vector<double>>& vectorCentroides, vector<vector<double>>& centroidesFinales, int k) {
+void reclusterKmeans2(vector<vector<double>>& vectorCentroides, vector<vector<double>>& centroidesFinales, int hilos, int k) {
 	int posR = 0;
 	posR = generarInt(0, vectorCentroides.size() - 1);
 	centroidesFinales.push_back(vectorCentroides[posR]);
@@ -144,13 +137,16 @@ void reclusterKmeans2(vector<vector<double>>& vectorCentroides, vector<vector<do
 	double fCosto = 0.0;
 
 	int tamCentroides = vectorCentroides.size();
+	#pragma omp parallel for num_threads(hilos) reduction(+:fCosto)
 	for (int i = 0; i < tamCentroides; ++i) {
 		fCosto += calcDisMin(vectorCentroides[i], centroidesFinales);
 	}
 	vector<double> probabilidades;
 	double prob = 0.0;
 	while (contador < k) {
-		probabilidadesPuntos(vectorCentroides, centroidesFinales,probabilidades, fCosto, 1);
+		probabilidades.clear();
+		probabilidades.resize(tamCentroides);
+		probabilidadesPuntos(vectorCentroides, centroidesFinales,probabilidades, fCosto, hilos, 1);
 		posR = generarInt(0, vectorCentroides.size() - 1);
 		prob = generarDouble(0, 1);
 		if (prob < probabilidades[posR]) {
@@ -158,6 +154,7 @@ void reclusterKmeans2(vector<vector<double>>& vectorCentroides, vector<vector<do
 				centroidesFinales.push_back(vectorCentroides[posR]);
 				++contador;
 				fCosto = 0;
+				#pragma omp parallel for num_threads(hilos) reduction(+:fCosto)
 				for (int i = 0; i < tamCentroides; ++i) {
 					fCosto += calcDisMin(vectorCentroides[i], centroidesFinales);
 				}
@@ -165,30 +162,34 @@ void reclusterKmeans2(vector<vector<double>>& vectorCentroides, vector<vector<do
 		}
 	}
 }
+
 void kmeansParallelInit(vector<vector<double>>& vectorDatos, vector<vector<double>>& vectorCentroides, vector<vector<vector<double>>>& valoresCentroides, int hilos, int m, int k) {
 	vector<double> probabilidades; //las probabilidades de cada x en X de ser escogido como centroide
-	vector<vector<double>> vCentroidesPrime;
 	double l = (double)k / 2;
 	double fCosto = 0.0;
 	int posicionR = generarInt(0, m - 1);
 	vectorCentroides.push_back(vectorDatos[posicionR]);
 
 	//Para calcular el costo inicial con el centroide inicial aleatorio
+	#pragma omp parallel for num_threads(hilos) reduction(+:fCosto)
 	for (int i = 0; i < m; ++i) {
 		fCosto += calcDisMin(vectorDatos[i], vectorCentroides);
 	}
 	int contador = 0;
 	double escoger = 0.0; // para escoger el numero random entre 0-1 y elegir un centroide si es menor a una prob
-	for (int i = 0; i < 5; ++i) {
-		probabilidadesPuntos(vectorDatos, vectorCentroides, probabilidades, fCosto, l);
-//# pragma omp parallel num_threads(hilos) shared(contador) private(escoger, posicionR)
+	for (int i = 0; i < 5; ++i) 
+	{
+		probabilidades.clear();
+		probabilidades.resize(m);
+		probabilidadesPuntos(vectorDatos, vectorCentroides, probabilidades, fCosto, hilos, l);
+		# pragma omp parallel num_threads(hilos) shared(contador, vectorCentroides) private(escoger, posicionR)
 		{
 			while (contador < l) {
 				escoger = generarDouble(0, 1);
 				posicionR = generarInt(0, m - 1);
 				if (escoger < probabilidades[posicionR]) {
 					if (validarCentroide(vectorCentroides, vectorDatos[posicionR])) {
-//# pragma omp critical
+						#pragma omp critical
 						{
 							vectorCentroides.push_back(vectorDatos[posicionR]);
 							++contador;
@@ -196,30 +197,16 @@ void kmeansParallelInit(vector<vector<double>>& vectorDatos, vector<vector<doubl
 					}
 				}
 			}
-			fCosto = 0;
-//# pragma omp single //Critical?
-			{
-				for (int i = 0; i < m; ++i) {
-					fCosto += calcDisMin(vectorDatos[i], vectorCentroides);
-				}
-			}
-			contador = 0;
 		}
+		fCosto = 0;
+		#pragma omp parallel for num_threads(hilos) reduction(+:fCosto)
+		for (int i = 0; i < m; ++i) {
+			fCosto += calcDisMin(vectorDatos[i], vectorCentroides);
+		}
+		contador = 0;
 	}
-	//En teoria aqui ya se tienen los centroides iniciales, ahora hay que calcular pesos por centroide y luego recluster
-	/*
-	int posMin = 0;
-# pragma omp parallel for num_threads(hilos)
-	for (int i = 0; i < m; ++i) {
-# pragma omp critical
-		{
-			posMin = calcMinPos(vectorDatos[i], vectorCentroides);
-			vector<double> lol = vectorDatos[i];
-			valoresCentroides[posMin].push_back(lol);
-		}
-	}*/
 	vector<vector<double>> centroidesFinales;
-	reclusterKmeans2(vectorCentroides,centroidesFinales, k);
+	reclusterKmeans2(vectorCentroides,centroidesFinales, hilos, k);
 	vectorCentroides.assign(centroidesFinales.begin(), centroidesFinales.end());
 	valoresCentroides.resize(vectorCentroides.size());
 }
@@ -244,29 +231,38 @@ int promediarGrupo(vector<vector<double>>& vectorGrupo, vector<double>& vectorPr
 	return cambio;
 }
 
-double algoritmoLloyd(vector<vector<double>>& vectorDatos, vector<vector<double>>& vectorCentroides, vector<vector<vector<double>>>& puntosAsociadosC, int m, int k, double eps) {
+double algoritmoLloyd(vector<vector<double>>& vectorDatos, vector<vector<double>>& vectorCentroides, vector<vector<vector<double>>>& puntosAsociadosC, int hilos, int m, int k, double eps) {
 	int posMin = 0;
 	double fCosto = 0.0;
 	int centroidesAlterados;
+	#pragma omp parallel for num_threads(hilos) shared(puntosAsociadosC) private(posMin) reduction(+:fCosto)
 	for (int i = 0; i < m; ++i) {
 		fCosto += calcDisMin(vectorDatos[i], vectorCentroides);
 		posMin = calcMinPos(vectorDatos[i], vectorCentroides);
-		puntosAsociadosC[posMin].push_back(vectorDatos[i]);
+		#pragma omp critical
+		{
+			puntosAsociadosC[posMin].push_back(vectorDatos[i]);
+		}
 	}
 	double fCostoPrime = fCosto;
 	do {
 		fCosto = fCostoPrime;
 		centroidesAlterados = 0;
+		#pragma omp parallel for num_threads(hilos) reduction(+:centroidesAlterados)
 		for (int i = 0; i < (int)puntosAsociadosC.size(); ++i) {
 			centroidesAlterados += promediarGrupo(puntosAsociadosC[i], vectorCentroides[i]);
 		}
 		puntosAsociadosC.clear();
 		puntosAsociadosC.resize(k);
 		fCostoPrime = 0;
+		#pragma omp parallel for num_threads(hilos) shared(puntosAsociadosC) private(posMin) reduction(+:fCostoPrime)
 		for (int i = 0; i < m; ++i) {
 			fCostoPrime += calcDisMin(vectorDatos[i], vectorCentroides);
 			posMin = calcMinPos(vectorDatos[i], vectorCentroides);
-			puntosAsociadosC[posMin].push_back(vectorDatos[i]);
+			#pragma omp critical
+			{
+				puntosAsociadosC[posMin].push_back(vectorDatos[i]);
+			}
 		}
 	} while ( ((fCosto - fCostoPrime) > eps) && (centroidesAlterados != 0) );
 	return fCostoPrime;
@@ -297,19 +293,11 @@ void escribirResultados(vector<vector<vector<double>>>& vGrupos, vector<vector<d
 			}
 			salida << endl;
 		}
-		salida << endl << endl;
+		salida << "Elementos: " << (int)vGrupos[i].size() << endl << endl;
 	}
 	salida << "Tiempo pared: "<< tPared <<"   Funcion de Costo: " << fCosto;
 	salida.close();
 }
-/*void imprimirVectores(int n, int m, vector<vector<double>>& datos) {
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << datos[i][j] << "\t";
-		}
-		cout << endl;
-	}
-}*/
 
 /*
 arg[1] = archivo de entrada
@@ -321,23 +309,21 @@ arg[6] = Epsilon de parada
 */
 int main(int argc, char** argv) {
 	if (argc == 7) {
-		srand(time(0));
+		srand((unsigned int)time(0));
 		int n = atoi(argv[2]);
 		int m = atoi(argv[3]);
 		int k = atoi(argv[4]);
 		int hilos = atoi(argv[5]);
-		double eps = atof(argv[6]);
-		int procesadores = omp_get_num_procs();
-		hilos *= procesadores;
+		double eps = atof(argv[6]);		
+		hilos *= omp_get_num_procs();
 		double fCosto = 0.0;
 		vector<vector<double>> datos(m, vector<double>(n));
 		vector<vector<double>> centroide;
 		vector<vector<vector<double>>> valoresAsociadosC (k);
 		cargarVectores(argv[1], n, m, datos);
-		//imprimirVectores(n, m, datos);
 		auto ini = omp_get_wtime();
 		kmeansParallelInit(datos, centroide, valoresAsociadosC, hilos, m, k);
-		fCosto = algoritmoLloyd(datos, centroide, valoresAsociadosC,  m, k, eps);
+		fCosto = algoritmoLloyd(datos, centroide, valoresAsociadosC, hilos, m, k, eps);
 		auto fin = omp_get_wtime();
 		escribirResultados(valoresAsociadosC,centroide, fCosto, (double)(fin-ini));
 	}
